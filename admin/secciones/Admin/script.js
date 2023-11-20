8f2f22d73aed88073fb2844d17b2321cd7340943
JDVERASTEGUI21 <133600858+JDVERASTEGUI21@users.noreply.github.com>
2023-11-20 15:28:08 -0500

    Delete admin/secciones directory

--- admin/secciones/Admin/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("abrirVentana").addEventListener("click", function() {
-    document.getElementById("ventanaFlotante").style.display = "block";
-});
-
-document.getElementById("cerrarVentana").addEventListener("click", function() {
-    document.getElementById("ventanaFlotante").style.display = "none";
-});
